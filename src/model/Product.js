69bc3dcbfbc5777966b759a8d850f62c197b927b
lucas11200 <lucas11200@hotmail.com>
2020-05-29 07:36:57 -0300

    Criando Order e Customer

---
+++ src/model/Product.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Product = new Schema({
+    sku: {
+        type: String,
+        required: [true, "SKU é obrigatório."],
+        trim: true,
+        index:true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: [true, "Nome é obrigatório."],
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, "Preço é obrigatório."],
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags: [{
+        type: String,
+        required: false,
+        trim: true
+    }],
+    createDate: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    updateDate: {
+        type: Date,
+        required: false
+    },
+});
+
+module.exports = mongoose.model('Product', Product);
---
+++ src/service/order.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Order = mongoose.model('Order');
+
+exports.create = async (body) =>{
+    let order = new Order();
+    order.customer = body.customer;
+    order.items = body.items;
+    order.status = body.status;
+
+    return await order.save();
+}
+
+exports.get = async (query) =>{
+    return await Order.find(query);
+}
