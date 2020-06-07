8697f93b52ab3b1747242fb8ebfe7047cffe1ebc
lucas11200 <lucas11200@hotmail.com>
2020-06-07 20:59:20 -0300

    d

--- src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = '';
-global.EMAIL_TMPL = 'Olá <strong>{0}</strong>! Seja bem vindo ao meu e-comerce, vc é top garai!';
-
-module.exports = {
-    connectionString: 'mongodb+srv://user:Fnb6fgnt@cluster0-hp9kg.mongodb.net/test?retryWrites=true&w=majority',
-    SENDGRID_API_KEY: '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',
-    containerConnectionString: 'TBD',
-}
