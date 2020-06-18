7cd4bb5fab67e4d311a792a3f38b18bd2096e8b7
lucas11200 <lucas11200@hotmail.com>
2020-06-17 21:51:43 -0300

    adicionando cors e limite do tamanho do arquivo

--- src/app.js
+++ src/app.js
@@ -19,8 +19,17 @@ const order = require("./route/order");
 const email = require("./route/email");
 
 //Convert body to json & Encode query parameter
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Resquested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', probe);
 app.use('/product', product);
